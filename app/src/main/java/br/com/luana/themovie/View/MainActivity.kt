873bfb93d7fe48ed112fa06b748717fb6dca3d08
luana-vargas <vargas.luana@outlook.com.br>
2020-08-26 15:29:14 -0300

    First Commit

---
+++ app/src/main/java/br/com/luana/themovie/View/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.luana.themovie.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.luana.themovie.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
